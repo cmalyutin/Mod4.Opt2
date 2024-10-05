072be44367be15e57f578c25ce9541e866375731
cmalyutin <christa.malyutin@csuglobal.edu>
2024-10-05 17:43:17 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scnr = new Scanner(System.in);
+
+        float grade1;
+        float grade2;
+        float grade3;
+        float grade4;
+        float grade5;
+        float grade6;
+        float grade7;
+        float grade8;
+        float grade9;
+        float grade10;
+        float minGrade;
+        float maxGrade;
+        float avgGrade;
+
+        System.out.println("Enter your grade: ");
+        grade1 = scnr.nextFloat();
+        System.out.println("Enter your grade: ");
+        grade2 = scnr.nextFloat();
+        System.out.println("Enter your grade: ");
+        grade3 = scnr.nextFloat();
+        System.out.println("Enter your grade: ");
+        grade4 = scnr.nextFloat();
+        System.out.println("Enter your grade: ");
+        grade5 = scnr.nextFloat();
+        System.out.println("Enter your grade: ");
+        grade6 = scnr.nextFloat();
+        System.out.println("Enter your grade: ");
+        grade7 = scnr.nextFloat();
+        System.out.println("Enter your grade: ");
+        grade8 = scnr.nextFloat();
+        System.out.println("Enter your grade: ");
+        grade9 = scnr.nextFloat();
+        System.out.println("Enter your grade: ");
+        grade10 = scnr.nextFloat();
+
+        for
+
+        System.out.println(grade1);13
+
+
+
+
+
+    }
+}
